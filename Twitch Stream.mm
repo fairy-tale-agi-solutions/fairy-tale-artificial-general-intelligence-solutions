93009812dcece61719c38857cd75ca30fa30d698
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-01-12 18:12:26 +0000

    Updated Twitch Stream mind map

--- Twitch Stream.mm
+++ Twitch Stream.mm
@@ -2,7 +2,7 @@
 <!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
 <node TEXT="Twitch&#xa;Stream" FOLDED="false" ID="ID_1656279608" CREATED="1544219747385" MODIFIED="1544248932852" STYLE="oval">
 <font SIZE="18"/>
-<hook NAME="MapStyle" zoom="0.909">
+<hook NAME="MapStyle" zoom="1.21">
     <properties fit_to_viewport="false" edgeColorConfiguration="#808080ff,#ff0000ff,#0000ffff,#00ff00ff,#ff00ffff,#00ffffff,#7c0000ff,#00007cff,#007c00ff,#7c007cff,#007c7cff,#7c7c00ff"/>
 
 <map_styles>
@@ -67,33 +67,36 @@
 <hook NAME="accessories/plugins/AutomaticLayout.properties" VALUE="ALL"/>
 <node TEXT="To Discuss" POSITION="left" ID="ID_1176508977" CREATED="1544219803920" MODIFIED="1544219808009">
 <edge COLOR="#ff0000"/>
-<node TEXT="Formalizing goals" ID="ID_190139588" CREATED="1544219962394" MODIFIED="1544219965558"/>
-<node TEXT="High level architectural diagram of existing AGI systems" ID="ID_1654077132" CREATED="1544222606259" MODIFIED="1544222618864"/>
-<node TEXT="Plan vs Prediction" ID="ID_1282336496" CREATED="1544222635200" MODIFIED="1544222639289"/>
-<node TEXT="High level feelings" FOLDED="true" ID="ID_222354344" CREATED="1544224952402" MODIFIED="1544233196918">
+<node TEXT="Formalizing goals - puffnfresh" ID="ID_190139588" CREATED="1544219962394" MODIFIED="1545308842270"/>
+<node TEXT="Formalizing decision processes - puffnfresh" ID="ID_807095631" CREATED="1545429776554" MODIFIED="1545429786712"/>
+<node TEXT="High level architectural diagram of existing AGI systems - Freeman" ID="ID_1654077132" CREATED="1544222606259" MODIFIED="1545308858031"/>
+<node TEXT="Plan and Prediction - Freeman" ID="ID_1282336496" CREATED="1544222635200" MODIFIED="1545308825620"/>
+<node TEXT="Causation and Correlation - Freeman" FOLDED="true" ID="ID_982672590" CREATED="1545308801237" MODIFIED="1545308817342">
+<node TEXT="Causation always implies correlation" ID="ID_1521747844" CREATED="1546636805684" MODIFIED="1546636814559"/>
+</node>
+<node TEXT="Understanding - Freeman" ID="ID_686425796" CREATED="1545397285770" MODIFIED="1545397290313"/>
+<node TEXT="Backwards reasoning for AGI creation - Freeman" ID="ID_1696006592" CREATED="1545429496162" MODIFIED="1545429503769"/>
+<node TEXT="High level feelings - Joe" FOLDED="true" ID="ID_222354344" CREATED="1544224952402" MODIFIED="1545308867173">
 <node TEXT="Anger" ID="ID_1813647205" CREATED="1544224960053" MODIFIED="1544224963666"/>
 <node TEXT="Excitement" ID="ID_479419584" CREATED="1544224963941" MODIFIED="1544224966343"/>
 <node TEXT="Fear" ID="ID_885280632" CREATED="1544224969128" MODIFIED="1544224970554"/>
 <node TEXT="Happiness &lt;-&gt;Sadness" ID="ID_1699378384" CREATED="1544224973590" MODIFIED="1544847621136"/>
 <node TEXT="Energy / Hunger" ID="ID_221629633" CREATED="1544225091277" MODIFIED="1544847631095"/>
 </node>
-<node TEXT="Defining General Intelligence" ID="ID_1964373822" CREATED="1544238750293" MODIFIED="1544238757406"/>
+<node TEXT="Defining General Intelligence - Matthias" ID="ID_1964373822" CREATED="1544238750293" MODIFIED="1545308875747"/>
 <node TEXT="How do we deal with perception when evaluating a plan and/or prediction - Joe" ID="ID_487717506" CREATED="1544233293815" MODIFIED="1544233305906"/>
+<node TEXT="AIRIS Architecture - Berick" ID="ID_1397684125" CREATED="1546636730973" MODIFIED="1546636759662"/>
+<node TEXT="How to have enough patience to allow AGI to grow-up? - Joe" ID="ID_1504636723" CREATED="1547252178230" MODIFIED="1547252181351"/>
+<node TEXT="allowing an AGI to not serve human desires - Joe" ID="ID_315712436" CREATED="1547252194574" MODIFIED="1547252196959"/>
+<node TEXT="how to have the patience to allow the AGI to make mistakes and not immediately think its a bug - Berick" ID="ID_1633166572" CREATED="1547252213433" MODIFIED="1547252216460"/>
 </node>
 <node TEXT="Discussed" POSITION="left" ID="ID_744557926" CREATED="1544812856611" MODIFIED="1544812859665">
 <edge COLOR="#ff00ff"/>
-<node TEXT="Embodied AGI - Ronald" ID="ID_1988867455" CREATED="1544827781422" MODIFIED="1544847456031"/>
-<node TEXT="Setting goals and plans. What motivates an AGI to grow - Joe" ID="ID_1517807969" CREATED="1544827457767" MODIFIED="1544827469159"/>
-<node TEXT="Intrinsic skill producing offspring - Joe" ID="ID_1477864484" CREATED="1544830270544" MODIFIED="1544830280233"/>
-<node TEXT="Intrinsic skill cooperation - Freeman" ID="ID_1211946261" CREATED="1544830420985" MODIFIED="1544830561436"/>
-<node TEXT="What is a life form - Ronald" ID="ID_1668165180" CREATED="1544832122545" MODIFIED="1544844007119" LINK="https://en.wikipedia.org/wiki/Life#Biology"/>
-<node TEXT="Consciousness and centers of power - Freeman" ID="ID_320192653" CREATED="1544824960062" MODIFIED="1544847438419"/>
-<node TEXT="2 cycles - SM &amp; planning - Joe" ID="ID_17370575" CREATED="1544845464706" MODIFIED="1544845500216"/>
-<node TEXT="AGI creating its own hardware - Joe" ID="ID_1148761542" CREATED="1544847422539" MODIFIED="1544847429903"/>
+<node TEXT="Project AGI metrics / qualities" ID="ID_331206838" CREATED="1547252162025" MODIFIED="1547252168894"/>
 </node>
-<node TEXT="History" FOLDED="true" POSITION="right" ID="ID_1674917923" CREATED="1544219810520" MODIFIED="1544812872989">
+<node TEXT="History" POSITION="right" ID="ID_1674917923" CREATED="1544219810520" MODIFIED="1544812872989">
 <edge COLOR="#ff0000"/>
-<node TEXT="07-12-2018" ID="ID_1967652407" CREATED="1544812882289" MODIFIED="1544812886102">
+<node TEXT="07-12-2018" FOLDED="true" ID="ID_1967652407" CREATED="1544812882289" MODIFIED="1544812886102">
 <node TEXT="Recalculation of a plan" ID="ID_966814247" CREATED="1544225733831" MODIFIED="1544225919444">
 <node TEXT="Cost of recalculation" ID="ID_877711523" CREATED="1544225932936" MODIFIED="1544225936877"/>
 <node TEXT="Affected by feelings" ID="ID_1236019835" CREATED="1544226184333" MODIFIED="1544226191310"/>
@@ -113,6 +116,33 @@
 </node>
 <node TEXT="Binary goal score function" ID="ID_1670706774" CREATED="1544239805971" MODIFIED="1544239819025"/>
 <node TEXT="It seems to me that the biggest problem in creating any AGI would be being able to have a scoring function that is intermediate to your final goal - Matt" ID="ID_525668096" CREATED="1544230072337" MODIFIED="1544240781455"/>
+</node>
+<node TEXT="14-12-2018" ID="ID_862218660" CREATED="1545165805042" MODIFIED="1545165808947">
+<node TEXT="Embodied AGI - Ronald" ID="ID_1988867455" CREATED="1544827781422" MODIFIED="1544847456031"/>
+<node TEXT="Setting goals and plans. What motivates an AGI to grow - Joe" ID="ID_1517807969" CREATED="1544827457767" MODIFIED="1544827469159"/>
+<node TEXT="Intrinsic skill producing offspring - Joe" ID="ID_1477864484" CREATED="1544830270544" MODIFIED="1544830280233"/>
+<node TEXT="Intrinsic skill cooperation - Freeman" ID="ID_1211946261" CREATED="1544830420985" MODIFIED="1544830561436"/>
+<node TEXT="What is a life form - Ronald" ID="ID_1668165180" CREATED="1544832122545" MODIFIED="1544844007119" LINK="https://en.wikipedia.org/wiki/Life#Biology"/>
+<node TEXT="Consciousness and centers of power - Freeman" ID="ID_320192653" CREATED="1544824960062" MODIFIED="1544847438419"/>
+<node TEXT="2 cycles - SM &amp; planning - Joe" ID="ID_17370575" CREATED="1544845464706" MODIFIED="1544845500216"/>
+<node TEXT="AGI creating its own hardware - Joe" ID="ID_1148761542" CREATED="1544847422539" MODIFIED="1544847429903"/>
+</node>
+<node TEXT="21-12-2018" ID="ID_1064188134" CREATED="1545944007765" MODIFIED="1545944011734">
+<node TEXT="Properties of realities which&#xa;are useful for AGIs - Freeman" ID="ID_213755958" CREATED="1545423060265" MODIFIED="1546030212000">
+<node TEXT="Time" ID="ID_1787697322" CREATED="1545423114732" MODIFIED="1545423116223"/>
+<node TEXT="Causality" ID="ID_1842585175" CREATED="1545423116794" MODIFIED="1545423118887"/>
+</node>
+<node TEXT="Freedom - Joe" ID="ID_1138933148" CREATED="1545433629327" MODIFIED="1545433632988"/>
+<node TEXT="Singularity NET - Joe" ID="ID_868482398" CREATED="1545438449472" MODIFIED="1545944023286"/>
+</node>
+<node TEXT="28-12-2018" ID="ID_1248624614" CREATED="1546633811551" MODIFIED="1546633817005">
+<node TEXT="AGI Safety" ID="ID_1072068277" CREATED="1546041013552" MODIFIED="1546041017854"/>
+</node>
+<node TEXT="04-01-2019" ID="ID_1032702328" CREATED="1547243474053" MODIFIED="1547243482659">
+<node TEXT="Motivation, Goal, Value - Joe" FOLDED="true" ID="ID_145376492" CREATED="1545437855815" MODIFIED="1546633878901">
+<node TEXT="Motivation ~ Goal - apparently" ID="ID_1588293372" CREATED="1546645418729" MODIFIED="1546645424702"/>
+<node TEXT="Motivation - how intense goal is pursued" ID="ID_1524747355" CREATED="1546648529057" MODIFIED="1546648541522"/>
+</node>
 </node>
 </node>
 </node>
