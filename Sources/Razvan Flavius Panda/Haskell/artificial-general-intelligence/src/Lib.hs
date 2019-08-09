42f9fa0624971d28b65b99d306f4cd58c4785194
Răzvan Flavius Panda <razvan.panda@gmail.com>
2019-08-09 01:31:50 +0100

    Haskell AGI humble beginnings

--- Sources/Razvan Flavius Panda/Haskell/artificial-general-intelligence/src/Lib.hs
+++ Sources/Razvan Flavius Panda/Haskell/artificial-general-intelligence/src/Lib.hs
@@ -1,6 +1,33 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+import Control.Monad
+
+run :: WorldState -> IO ()
+run startingWorldState =
+  when (goalAchieved startingWorldState == Searching)
+    $ run (action startingWorldState)
+
+goalAchieved :: WorldState -> GoalAchievementState
+goalAchieved worldState = undefined
+
+action :: WorldState -> WorldState
+action worldState = undefined
+
+data WorldState = WorldState {
+  time :: Time,
+  externalState :: ExternalState,
+  internalState :: InternalState
+}
+
+data Time = Time
+
+data ExternalState = ExternalState
+
+data InternalState = InternalState {
+  goal :: Goal,
+  worldLine :: [WorldState]
+}
+
+data Goal = Goal
+
+data GoalAchievementState = Achieved | Failed | Searching deriving Eq
